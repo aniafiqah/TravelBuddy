f236ab8ea5e2f16a0b81f254f9ada74f3b0baae1
arikashika <75994833+nurqhaisara@users.noreply.github.com>
2021-01-10 18:17:11 +0800

    review

--- homepage.js
+++ homepage.js
@@ -37,3 +37,15 @@ function on() {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+
+function genReview() {
+  var randNum = Math.floor(Math.random() * 4) + 1;
+  document.getElementById('review').innerHTML = quotes[randNum];
+}
+
+//review array
+var quotes = ["Blank",
+"\"The hotel was simple amazing and I couldn't thank Travel Buddy more for helping out. I've been a customer for awhile and I have to say it's probably my favourite web!\"<br>-  Rahim from Terengganu",
+"\"Looks exactly what is posted here in Travel Buddy. Great service!\"<br> - John Malik from Kedah",
+"\"I could not have asked for a better stay than Lexis Hibiscus. Thanks to Travel Buddy.\"<br>- Mark Lee from Sarawak",
+"\"Had an excellent excellent recommendations!\"<br>- Mutusami from KL",];
